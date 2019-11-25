84550f22a3ffefc52a30371d9df7c90c1e20e72e
emperorhan <iny27@naver.com>
2019-11-26 01:02:40 +0900

    fix: word err

--- contracts/led.system/src/producer.cpp
+++ contracts/led.system/src/producer.cpp
@@ -420,7 +420,7 @@ namespace eosiosystem {
       auto new_vote_weight = stake2vote( voter->staked );
       int64_t new_staked = voter->staked;
       
-      if (!interoirs.size() && !proxy) {
+      if (!interiors.size() && !proxy) {
          _gstate.total_vote_amount -= voter->last_vote;
          new_staked = 0;
       } else {
